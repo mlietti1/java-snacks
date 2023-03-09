a8bae827abb203c53e35fd4a3f018bcefe69b4c3
mlietti1 <mcf.lietti@outlook.com>
2023-03-09 14:21:34 +0100

    init

---
+++ src/org/lessons/java/Snack1.java
@@ -0,0 +1,16 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int num = Integer.parseInt(scan.nextLine());
+        if(num % 2 == 0){
+            System.out.println(num + " è un numero pari.");
+        }else{
+            System.out.println(num + "è un numero dispari.");
+        }
+    }
+}
