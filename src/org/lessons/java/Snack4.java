94741fa047beb4b98ee23ecfe8bc95f017585782
mlietti1 <mcf.lietti@outlook.com>
2023-03-09 16:09:41 +0100

    snack4

--- src/org/lessons/java/Snack4.java
+++ src/org/lessons/java/Snack4.java
@@ -12,31 +12,23 @@ public class Snack4 {
         System.out.print("Length of the second array: ");
         int arrLength2 = Integer.parseInt(scan.nextLine());
 
-        String[] array1 = new String[arrLength1];
-        String[] array2 = new String[arrLength2];
-        String[] array3 = {"purple", "blue", "green", "yellow", "orange", "red", "pink", "white", "black", "brown"};
-
-
-
-        if(array1.length > array2.length){
-            int lengthDiff = array1.length - array2.length;
-            System.out.println("Length difference: " + lengthDiff);
-            for(int i = 0; i < lengthDiff; i++){
-                int index = new Random().nextInt(array3.length);
-                String newItem = array3[index];
-                System.out.println(i+1 + ": " + newItem);
-            }
-        } else if (array1.length < array2.length) {
-            int lengthDiff = array2.length - array1.length;
-            System.out.println("Length difference: " + lengthDiff);
-            for(int i = 0; i < lengthDiff; i++){
-                int index = new Random().nextInt(array3.length);
-                String newItem = array3[index];
-                System.out.println(i+1 + ": " + newItem);
-            }
-        }else {
+        if(arrLength1 == arrLength2){
             System.out.println("The two arrays have the same length.");
+        }else {
+            String[] longer = new String[Math.max(arrLength1, arrLength2)];
+            String[] shorter = new String[Math.min(arrLength1, arrLength2)];
+            String[] arrayCustom = {"purple", "blue", "green", "yellow", "orange", "red", "pink", "white", "black", "brown"};
+
+            int lengthDiff = longer.length - shorter.length;
+
+            for(int i = 0; i < lengthDiff; i++) {
+                int index = new Random().nextInt(arrayCustom.length);
+                String newItem = arrayCustom[index];
+                System.out.println(i + 1 + ": " + newItem);
+
+            }
         }
+
         scan.close();
     }
 }
